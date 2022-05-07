0705fffe4d6849757a25a23d915833bf5689dd9e
Dmitry <29678249+ZemlyakovDmitry@users.noreply.github.com>
2022-05-07 20:28:21 +0300

    Add files via upload

---
+++ startcmd/startcmd.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <shlobj.h>
+
+int main()
+{
+    if (IsUserAnAdmin()) {
+        MessageBoxA(NULL, "poxeked, i have admin privileges", "poxek", MB_ICONWARNING);
+    }
+    else {
+        MessageBoxA(NULL, "poxeked, but i don\'t have admin privileges", "poxek", MB_ICONERROR);
+    }
+}
